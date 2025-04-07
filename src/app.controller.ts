bf98819180764b716562252fa909a325b86bdbb3
naveenbutolatech <naya.butola@email.com>
2025-04-07 17:24:19 +0530

    Update changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log("working...with mongodb hello");
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! again... from mongodb';
+    return 'Hello World! naveen butols';
   }
 }
